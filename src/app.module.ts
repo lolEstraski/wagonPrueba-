e94dc1d948eba11a7f068c1102c766cd01459d73
Carlos Andres Ortegon Tique <carlos1andres20@gmail.com>
2025-08-07 08:24:50 -0500

    Configuracion exitosa del orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { configService,  } from './config/config.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TasksModule } from './tasks/tasks.module';
+import { Task } from './tasks/entities/task.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get('DB_USERNAME', 'postgres'),
+        password: configService.get('DB_PASSWORD', 'password'),
+        database: configService.get('DB_NAME', 'task_manager'),
+        entities: [Task],
+        synchronize: configService.get('NODE_ENV') !== 'production',
+        logging: configService.get('NODE_ENV') === 'development',
+      }),
+      inject: [ConfigService],
+    }),
+    TasksModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, ConfigService]
 })
-export class AppModule { }
+export class AppModule {}
