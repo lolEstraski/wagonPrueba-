daae0b82814131c3e75570a24669e67ccc44add2
Carlos Andres Ortegon Tique <carlos1andres20@gmail.com>
2025-08-07 02:39:09 -0500

    se trata de configurar para  que arranque la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { configService } from './config/config.service';
+import { configService,  } from './config/config.service';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig())
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService]
 })
 export class AppModule { }
